344849ae5b4a5f7ddc143776392f69a964aaf46a
Wane mar <wane.75@outlook.com>
2019-03-02 14:50:19 +0100

    Unique cursor + current note detection

--- Test_svg.js/wasco/grille.js
+++ Test_svg.js/wasco/grille.js
@@ -7,8 +7,6 @@ function getRandomColor() {
     return color
 }
 
-
-
 var durationArray
 
 function readDurationFile(e) {
@@ -24,10 +22,8 @@ function readDurationFile(e) {
     reader.readAsText(file)
 }
 
-
-
-
 var partition
+
 function readSingleFile(e) {
     var file = e.target.files[0]
     if (!file) {
@@ -42,26 +38,45 @@ function readSingleFile(e) {
     reader.readAsText(file)
 }
 
-
-
-
 function parse(contents) {
     return JSON.parse(contents)
 }
 
+function dichotomie(timeLine, tab) {
+
+    if (tab.length == 1) {
+
+        if (timeLine >= tab[0].x_init && timeLine < tab[0].x_end) {
+
+            console.log("Note found !!")
+            return SVG.get(tab[0].x_init)
+
+        } else {
+
+            return "Note not found !!"
+
+        }
+    } else {
+
+        if (timeLine < tab[parseInt(tab.length / 2)].x_init) {
+
+            return dichotomie(timeLine, tab.slice(0, parseInt(tab.length / 2)))
+
+        } else {
+
+            return dichotomie(timeLine, tab.slice(parseInt(tab.length / 2)))
+
+        }
+    }
+}
+
 document.getElementById('file-input').addEventListener('change', readSingleFile, false)
-
-
 
 document.getElementById('real_duration').addEventListener('change', readDurationFile, false)
 
-
-
-
-
 function run(partition, durationArray) {
 
-    /* Pres Calcul*/
+    /* Pre Calcul*/
     var height = 12 * 9 * 30
     var width = 500
     var duration = 0
@@ -74,20 +89,24 @@ function run(partition, durationArray) {
         } else if (partition[i][0].TRILL) {
             duration += partition[i][1].duration
             width += 120 * partition[i][1].duration * partition[i][0].TRILL.length
-        }else if(partition[i][0].CHORD){
+        } else if (partition[i][0].CHORD) {
             duration += partition[i][1].duration
             width += 120 * partition[i][1].duration
         }
     }
 
-    // fin du pres calcul
+    // fin du pre calcul
 
     var widthNote = 80
     var heightNote = 30
 
     var draw = SVG('drawing').size(width, height)
-    var xLine = draw.line(50, 0, 50, height).stroke({ width: 1 }).id('xLine')
-    var yLine = draw.line(50, height, width, height).stroke({ width: 1 }).id('yLine')
+    var xLine = draw.line(50, 0, 50, height).stroke({
+        width: 1
+    }).id('xLine')
+    var yLine = draw.line(50, height, width, height).stroke({
+        width: 1
+    }).id('yLine')
 
     var note = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 
@@ -117,76 +136,34 @@ function run(partition, durationArray) {
             var keyB = draw.rect(100, 30).move(0, i * 30).fill('black').id(key)
             var rectPaire = draw.rect(width, 30).move(100, i * 30).fill('#e5e5e5')
             var text = draw.text(key)
-            text.move(40, i * 30).font({ fill: '#fff', family: 'Arial' })
+            text.move(40, i * 30).font({
+                fill: '#fff',
+                family: 'Arial'
+            })
         } else {
             var keyW = draw.rect(100, 30).move(0, i * 30).fill('#e1e1e1').id(key)
             var rectImpaire = draw.rect(width, 30).move(100, i * 30).fill('#f5f2d5')
             var text = draw.text(key)
-            text.move(40, i * 30).font({ fill: '#000', family: 'Arial' })
+            text.move(40, i * 30).font({
+                fill: '#000',
+                family: 'Arial'
+            })
         }
     }
 
     var sommeNoteDuration = 100
-    var lastDuration = 0
-    var timeLineTab = []
+    var rectPositionTab = []
 
+    var intervalID = window.addEventListener("click", function () { // Simulate Event
 
+        console.log("timeline :: " + timeLine.attr('x1'))
+        var timeL = timeLine.attr('x1')
+        console.log("tileLne Tb : " + rectPositionTab.length)
+        var currentNote = dichotomie(timeL, rectPositionTab)
+        console.log(currentNote)
+        timeLine.pause()
 
-    var intervalID = window.addEventListener("click",function(){
-        var newTimeLineTab = []
-        var ligneActive
-        var tmp=[]
-        var lignes = document.getElementsByTagName("line")
-        for (var i = 0; i<lignes.length; i++) {
-            if (lignes[i].getAttribute('stroke-width') == undefined || lignes[i].getAttribute('stroke-width') != 1) {
-                tmp.push(lignes[i])
-            }
-        }
-        if (tmp.length>0) {
-            ligneActive = tmp[tmp.length - 1]
-        }else{
-            ligneActive = lignes[0]
-        }
-        
-        
-        i = 0
-        index = 0
-        while (i<timeLineTab.length) {
-            l = document.getElementById(timeLineTab[i].line.id())
-            if (l.getAttribute("x1") == ligneActive.getAttribute("x1")) {
-                index = i
-            }
-            document.getElementsByTagName("svg")[0].removeChild(l)
-            i++;
-        }
-        i = index + 1
-        attente = 0.0
-        
-        while (i<timeLineTab.length) {
-            var realDuration = durationArray[i%durationArray.length]
-            
-            var newLigne = draw.line(timeLineTab[i].x_init, 0, timeLineTab[i].x_init, height)
-                            .stroke({ width: 1 })
-                            .id("ligne"+i+"_"+Math.random())
-                            
-            newLigne.animate({ duration : realDuration , ease: '-', delay: attente / 1000+'s' })
-                            .attr({'stroke-width': 1})
-                            .move(timeLineTab[i].x_end,0)
-                            .after(function(situation) {
-                                this.animate().attr({ 'stroke-width': 0})
-                            })
-            
-            newTimeLineTab.push({line : newLigne, x_init : timeLineTab[i].x_init, x_end : timeLineTab[i].x_end})
-            
-            attente += realDuration
-            
-            i++
-        }
-        
-        timeLineTab = newTimeLineTab;
-        
     })
-
 
     for (var i = 1; i < partition.length - 1; i++) {
         var rectY = 0
@@ -194,133 +171,147 @@ function run(partition, durationArray) {
 
         if (partition[i][0].NOTE != undefined && partition[i][0].NOTE != 0) { // NOTE
 
-            var timeLine = draw.line(sommeNoteDuration, 0, sommeNoteDuration, height).stroke({ width: 1 }).id("note"+i)
-            timeLine.animate({ duration : partition[i][1].duration * 1000, ease: '-', delay: lastDuration+'s' })
-                            .attr({'stroke-width': 1})
-                            .move(sommeNoteDuration + 120*partition[i][1].duration, 0)
-                            .after(function(situation) {
-                                    this.animate().attr({ 'stroke-width': 0})
-                             }) 
-            
-            lastDuration += partition[i][1].duration
-            timeLineTab.push({line : timeLine, x_init : sommeNoteDuration, x_end : sommeNoteDuration + 120*partition[i][1].duration})
-            
+            rectPositionTab.push({
+                x_init: sommeNoteDuration,
+                x_end: sommeNoteDuration + 120 * partition[i][1].duration,
+                duree_note: partition[i][1].duration
+            })
 
             rectY = document.getElementById(midiMatrix[partition[i][0].NOTE]).getAttribute("y")
-            
+
             if (partition[i][1].duration == 0) { // Grace note
-                var rect = draw.rect(30, 30).fill("#000").move(sommeNoteDuration, rectY).radius(50);
+
+                var rect = draw.rect(30, 30)
+                    .fill("#000")
+                    .move(sommeNoteDuration, rectY)
+                    .radius(50)
+
                 sommeNoteDuration += 30
+
             } else {
-                var rect = draw.rect(120 * partition[i][1].duration, 30).fill(getRandomColor()).move(sommeNoteDuration, rectY);
+
+                var rect = draw.rect(120 * partition[i][1].duration, 30)
+                    .fill(getRandomColor())
+                    .move(sommeNoteDuration, rectY)
+                    .id(sommeNoteDuration)
+
                 sommeNoteDuration += 120 * partition[i][1].duration
+
             }
 
         } else if (partition[i][0].TRILL) { // TRILL
-        
+
             var color = getRandomColor()
-            //console.log(partition[i][0])
-            var timeLine = draw.line(sommeNoteDuration, 0, sommeNoteDuration, height).stroke({ width: 1 }).id("trill"+i)
-            timeLine.animate({ duration : partition[i][1].duration * 1000, ease: '-', delay: lastDuration+'s' })
-                                .attr({'stroke-width': 1,
-                                })
-                                .move(sommeNoteDuration + 120*partition[i][1].duration, 0)
-                                .after(function(situation) {
-                                    this.animate().attr({ 'stroke-width': 0})
-                                }) 
-            lastDuration += partition[i][1].duration
-            timeLineTab.push({line : timeLine, x_init : sommeNoteDuration, x_end : sommeNoteDuration + 120*partition[i][1].duration})
+
+            rectPositionTab.push({
+                x_init: sommeNoteDuration,
+                x_end: sommeNoteDuration + 120 * partition[i][1].duration,
+                duree_note: partition[i][1].duration
+            })
 
             // TRILL qui contient des CHORD
 
             var trill = partition[i][0].TRILL
             var nbNote = 0
-            
-            for(var k = 0; k<trill.length; k++){
-                if(trill[k].CHORD){
+
+            for (var k = 0; k < trill.length; k++) {
+                if (trill[k].CHORD) {
                     nbNote += trill[k].CHORD.length
-                }else{
+                } else {
                     nbNote++
                 }
             }
 
-            for(var k = 0; k< trill.length; k++){
-                if(trill[k].CHORD){
+            for (var k = 0; k < trill.length; k++) {
+
+                if (trill[k].CHORD) {
+
                     var chord = trill[k].CHORD
-                    for(var d = 0; d<chord.length; d++){
+                    for (var d = 0; d < chord.length; d++) {
                         rectY = document.getElementById(midiMatrix[chord[d]]).getAttribute("y")
-                        var rect = draw.rect(120 * (partition[i][1].duration/nbNote), 30).fill(color).move(sommeNoteDuration, rectY)
+                        var rect = draw.rect(120 * (partition[i][1].duration / nbNote), 30)
+                            .fill(color)
+                            .move(sommeNoteDuration, rectY)
+                            .id(sommeNoteDuration)
                     }
-                    sommeNoteDuration += 120 * partition[i][1].duration/nbNote
-                }else{ 
+
+                    sommeNoteDuration += 120 * partition[i][1].duration / nbNote
+
+                } else {
 
                     rectY = document.getElementById(midiMatrix[trill[k]]).getAttribute("y")
 
                     if (partition[i][1].duration == 0) { // Grace note
-                        var rect = draw.rect(30, 30).fill("#000").move(sommeNoteDuration, rectY).radius(50)
+
+                        var rect = draw.rect(30, 30).fill("#000")
+                            .move(sommeNoteDuration, rectY)
+                            .radius(50)
+                            .id(sommeNoteDuration);
+
                         sommeNoteDuration += 30
+
                     } else {
-                        var rect = draw.rect(120 * (partition[i][1].duration/trill.length), 30).fill(color).move(sommeNoteDuration, rectY)
-                        sommeNoteDuration += 120 * (partition[i][1].duration/trill.length)
+
+                        var rect = draw.rect(120 * (partition[i][1].duration / trill.length), 30)
+                            .fill(color)
+                            .move(sommeNoteDuration, rectY)
+                            .id(sommeNoteDuration);
+
+                        sommeNoteDuration += 120 * (partition[i][1].duration / trill.length)
                     }
                 }
             }
-            
-        } else if(partition[i][0].CHORD){
+
+        } else if (partition[i][0].CHORD) {
+
             var color = getRandomColor()
             var chord = partition[i][0].CHORD
             var duree = partition[i][1].duration
-            for(var k = 0; k<chord.length; k++){
+            for (var k = 0; k < chord.length; k++) {
 
                 rectY = document.getElementById(midiMatrix[chord[k]]).getAttribute("y")
-                var rect = draw.rect(120 * duree, 30).fill(color).move(sommeNoteDuration, rectY)
+                var rect = draw.rect(120 * duree, 30)
+                    .fill(color)
+                    .move(sommeNoteDuration, rectY)
+                    .id(sommeNoteDuration)
             }
 
-            var timeLine = draw.line(sommeNoteDuration, 0, sommeNoteDuration, height).stroke({ width: 1 }).id("chord"+i)
-            timeLine.animate({ duration : partition[i][1].duration * 1000, ease: '-', delay: lastDuration+'s' })
-                            .attr({
-                                'stroke-width': 1, 
-                            })
-                            .move(sommeNoteDuration + 120*partition[i][1].duration, 0)
-                            .after(function(situation) {
-                                  this.animate().attr({ 'stroke-width': 0})
-                            }) 
-            lastDuration += partition[i][1].duration
-
-            timeLineTab.push({line : timeLine, x_init : sommeNoteDuration, x_end : sommeNoteDuration + 120*partition[i][1].duration})
+            rectPositionTab.push({
+                x_init: sommeNoteDuration,
+                x_end: sommeNoteDuration + 120 * partition[i][1].duration,
+                duree_note: partition[i][1].duration
+            })
 
             sommeNoteDuration += 120 * duree
 
         } else if (partition[i][0].NOTE == 0) { // silence
 
-            // var timeLine = draw.line(sommeNoteDuration, 0, sommeNoteDuration, height).stroke({ width: 0 })
-            // timeLine.animate({ duration : partition[i][1].duration * 1000, ease: '-', delay: lastDuration+'s' }).attr({'stroke-width': 1, 
-            //                                                                     x1: sommeNoteDuration + 120*partition[i][1].duration,
-            //                                                                     x2: sommeNoteDuration + 120*partition[i][1].duration}) 
-            // lastDuration += partition[i][1].duration
-            
+            var rect = draw.rect(120 * partition[i][1].duration, height)
+                .fill("#fff")
+                .move(sommeNoteDuration, rectY)
+                .id(sommeNoteDuration)
 
-            // document.getElementById("chord"+i).addEventListener("animationend",function (e) {
-            //     console.log(e)
-            // })
-            // timeLine.addEventListener("animationend",function (e) {
-            //     console.log(e)
-            // })
+            rectPositionTab.push({
+                x_init: sommeNoteDuration,
+                x_end: sommeNoteDuration + 120 * partition[i][1].duration,
+                duree_note: partition[i][1].duration
+            })
 
-
-            
-            var rect = draw.rect(120 * partition[i][1].duration, height).fill("#fff").move(sommeNoteDuration, rectY)
             sommeNoteDuration += 120 * partition[i][1].duration
-            
+
         }
     }
 
     // Cursor
-    // var timeLine = draw.line(100, 0, 100, height).stroke({ width: 0 }).id('timeLine')
-    // timeLine.animate(duration * 1000, '-', 0).attr({ x1: width, x2: width })
-    
+    var timeLine = draw.line(100, 0, 100, height).stroke({
+        width: 1
+    }).id('timeLine')
+    timeLine.animate(duration * 1000, '-', 0).attr({
+        x1: width,
+        x2: width
+    })
+
     //scrollTo(0, document.getElementById("timeLine").getAttribute("x"))
     scrollTo(0, document.getElementById("F#5").getAttribute("y"))
-
 
 }
